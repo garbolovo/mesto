929a1d6fc758e52ca0356061af6c612d76130d09
leva <qqq0411@yandex.ru>
2021-02-18 23:37:43 +0300

    done-1

--- scripts/script.js
+++ scripts/script.js
@@ -8,48 +8,59 @@ const saveForm = document.querySelector('.popup__submit-button');
 const nameInput = document.querySelector('.popup__name');
 const positionInput = document.querySelector('.popup__position');
 
+// like element
+const likes = document.querySelectorAll('.elements__like');
+
 function settingUpProfile() {
-    if (localStorage.getItem('name') !== null) {
-        profileName.textContent = localStorage.getItem('name');
-        nameInput.value = profileName.textContent;
+  if (localStorage.getItem('name') !== null) {
+    profileName.textContent = localStorage.getItem('name');
+    nameInput.value = profileName.textContent;
 
-    } else {
-        nameInput.value = profileName.textContent;
-    }
+  } else {
+    nameInput.value = profileName.textContent;
+  }
 
-    if (localStorage.getItem('position') !== null) {
-        profilePosition.textContent = localStorage.getItem('position');
-        positionInput.value = profilePosition.textContent;
+  if (localStorage.getItem('position') !== null) {
+    profilePosition.textContent = localStorage.getItem('position');
+    positionInput.value = profilePosition.textContent;
 
-    } else {
-        positionInput.value = profilePosition.textContent;
-    }
+  } else {
+    positionInput.value = profilePosition.textContent;
+  }
 
 }
 
 settingUpProfile();
 
 
-editBtn.addEventListener('click', (ev) => {
-    popup.classList.add('popup_shown');
-    settingUpProfile();
+editBtn.addEventListener('click', (event) => {
+  popup.classList.add('popup_shown');
+  settingUpProfile();
 
 })
 
-closePopup.addEventListener('click', (ev) => {
-    popup.classList.remove('popup_shown');
+closePopup.addEventListener('click', (event) => {
+  popup.classList.remove('popup_shown');
 })
 
-saveForm.addEventListener('click', (ev) => {
-    ev.preventDefault();
-    popup.classList.remove('popup_shown');
+saveForm.addEventListener('click', (event) => {
+  ev.preventDefault();
+  popup.classList.remove('popup_shown');
 
-    localStorage.setItem('name', nameInput.value);
-    profileName.textContent = nameInput.value;
+  localStorage.setItem('name', nameInput.value);
+  profileName.textContent = nameInput.value;
 
-    localStorage.setItem('position', positionInput.value);
-    profilePosition.textContent = positionInput.value;
+  localStorage.setItem('position', positionInput.value);
+  profilePosition.textContent = positionInput.value;
 
+})
+
+
+//like logic
+likes.forEach(function(like) {
+  like.addEventListener('click', function(event) {
+    this.classList.toggle('elements__like_is-liked');
+  })
 })
 
 
