792bfcb4b034186695834a4cf1d94b7b05cb4267
leva <qqq0411@yandex.ru>
2021-02-21 23:08:32 +0300

    formatted

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,32 @@
 //dom_els
-const editBtn = document.querySelector('.profile__edit-button');
-const profileName = document.querySelector('.profile__name');
-const profilePosition = document.querySelector('.profile__position');
-const popup = document.querySelector('.popup');
-const closePopup = document.querySelector('.popup__close');
-const saveForm = document.querySelector('.popup__submit-button');
-const nameInput = document.querySelector('.popup__name');
-const positionInput = document.querySelector('.popup__position');
-
+const editBtn = document.querySelector(".profile__edit-button");
+const profileName = document.querySelector(".profile__name");
+const profilePosition = document.querySelector(".profile__position");
+const popup = document.querySelector(".popup");
+const closePopup = document.querySelector(".popup__close");
+const saveForm = document.querySelector(".popup__submit-button");
+const nameInput = document.querySelector(".popup__name");
+const positionInput = document.querySelector(".popup__position");
 
 nameInput.value = profileName.textContent;
 positionInput.value = profilePosition.textContent;
 
+editBtn.addEventListener("click", (event) => {
+  popup.classList.add("popup_shown");
+});
 
-editBtn.addEventListener('click', (event) => {
-  popup.classList.add('popup_shown');
+closePopup.addEventListener("click", (event) => {
+  popup.classList.remove("popup_shown");
+});
 
-})
-
-closePopup.addEventListener('click', (event) => {
-  popup.classList.remove('popup_shown');
-})
-
-saveForm.addEventListener('click', (event) => {
+saveForm.addEventListener("click", (event) => {
   event.preventDefault();
-  popup.classList.remove('popup_shown');
+  popup.classList.remove("popup_shown");
 
   profileName.textContent = nameInput.value;
 
   profilePosition.textContent = positionInput.value;
-
-})
-
+});
 
 //like logic
 // likes.forEach(function (like) {
@@ -39,5 +34,3 @@ saveForm.addEventListener('click', (event) => {
 //     this.classList.toggle('elements__like_is-liked');
 //   })
 // })
-
-
