697e9a3175f9e0aa979a5788d05bd9d1997f0ada
Aleksei <leva@MacBook-Pro-Aleksei.local>
2021-03-08 01:57:54 +0300

    script.js updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,4 @@
 //dom_els
-<<<<<<< HEAD
-const editBtn = document.querySelector(".profile__edit-button");
-const profileName = document.querySelector(".profile__name");
-const profilePosition = document.querySelector(".profile__position");
-const popup = document.querySelector(".popup");
-const closePopup = document.querySelector(".popup__close");
-const saveForm = document.querySelector(".popup__submit-button");
-const nameInput = document.querySelector(".popup__name");
-const positionInput = document.querySelector(".popup__position");
-=======
 const editBtn = document.querySelector('.profile__edit-button');
 const profileName = document.querySelector('.profile__name');
 const profilePosition = document.querySelector('.profile__position');
@@ -19,23 +9,8 @@ const nameInput = document.querySelector('.popup__input-name');
 const positionInput = document.querySelector('.popup__input-position');
 
 const forma = document.querySelector('.popup__edit-form');
->>>>>>> 05dd4a5a8dd29c26d6812330b61b5a16394caf2c
 
 
-<<<<<<< HEAD
-editBtn.addEventListener("click", (event) => {
-  popup.classList.add("popup_shown");
-});
-
-closePopup.addEventListener("click", (event) => {
-  popup.classList.remove("popup_shown");
-});
-
-saveForm.addEventListener("click", (event) => {
-  event.preventDefault();
-  popup.classList.remove("popup_shown");
-
-=======
 function popupToClose() {
 	popup.classList.remove('popup_shown');
 }
@@ -54,21 +29,15 @@ function getUserData() {
 
 function saveData(e) {
   e.preventDefault();
->>>>>>> 05dd4a5a8dd29c26d6812330b61b5a16394caf2c
   profileName.textContent = nameInput.value;
   profilePosition.textContent = positionInput.value;	
   popupToClose();
 }
 
-<<<<<<< HEAD
-  profilePosition.textContent = positionInput.value;
-});
-=======
 editBtn.addEventListener('click', getUserData);
 closePopup.addEventListener('click', popupToClose);
 forma.addEventListener('submit', saveData)
 
->>>>>>> 05dd4a5a8dd29c26d6812330b61b5a16394caf2c
 
 //like logic
 // likes.forEach(function (like) {
@@ -76,3 +45,5 @@ forma.addEventListener('submit', saveData)
 //     this.classList.toggle('elements__like_is-liked');
 //   })
 // })
+
+
